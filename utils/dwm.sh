d6803e77e4271d792fbd0816901e9ccc8c21c368
Aquinn Edquist <61575417+Aqu1nEq@users.noreply.github.com>
2024-07-05 16:45:36 +0600

    dwm

--- utils/dwm.sh
+++ utils/dwm.sh
@@ -1,1 +1,30 @@
 # needs work
+
+mkdir -p ~/sources/suckless
+cd ~/sources/suckless
+
+git clone https://git.suckless.org/dwm
+git clone https://git.suckless.org/dmenu
+git clone https://git.suckless.org/st
+
+cd ~/sources/suckless/dwm
+make clean install
+
+cd ~/sources/suckless/dmenu
+make clean install
+
+cd ~/sources/suckless/st
+make clean install
+
+cd ~
+
+# Check if the file does not exist
+if [ ! -f "~/.xinitrc" ]; then
+    touch .xinitrc
+else
+    echo "File already exists"
+fi
+
+touch .xinitrc
+
+sed -i '$a exec dwm ' ~/.xinitrc
